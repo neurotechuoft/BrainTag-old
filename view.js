201788b3a9cb246e4e16db1431b70a4f025b24b9
Nick <nick.lesho@gmail.com>
2018-07-15 18:13:19 -0400

    added interactive graph and buttons

---
+++ view.js
@@ -0,0 +1,33 @@
+document.getElementById("zero").onclick = () => {
+    alert('clicked 0');   
+}
+document.getElementById("one").onclick = () => {
+    alert('clicked 1');	
+}
+document.getElementById("two").onclick = () => {
+    alert('clicked 2');   
+}
+document.getElementById("three").onclick = () => {
+    alert('clicked 3');   
+}
+document.getElementById("four").onclick = () => {
+    alert('clicked 4');   
+}
+document.getElementById("five").onclick = () => {
+    alert('clicked 5');   
+}
+document.getElementById("six").onclick = () => {
+	alert('clicked 6');
+}
+document.getElementById("seven").onclick = () => {
+	alert('clicked 7');
+}
+document.getElementById("eight").onclick = () => {
+    alert('clicked 8');   
+}
+document.getElementById("nine").onclick = () => {
+    alert('clicked 9');	
+}
+
+
+
